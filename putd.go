d97dc6fff8266c053db3f4de7737228272837aed
ushi <ushi@honkgong.info>
2017-03-31 02:03:43 +0200

    add metrics endpoint

--- putd.go
+++ putd.go
@@ -7,6 +7,8 @@ import (
 	"github.com/minio/minio-go"
 	"github.com/minio/minio-go/pkg/policy"
 	"github.com/pborman/uuid"
+	"github.com/rcrowley/go-metrics"
+	"io"
 	"net"
 	"net/http"
 	"net/url"
@@ -17,23 +19,19 @@ import (
 )
 
 var (
-	rootDir     string
-	listenAddr  string
-	s3Endpoint  string
-	s3Region    string
-	s3Bucket    string
-	s3AccessKey string
-	s3SecretKey string
+	rootDir    string
+	listenAddr string
+	s3         S3
 )
 
 func init() {
 	flag.StringVar(&rootDir, "root", "", "http root directory (default: .)")
 	flag.StringVar(&listenAddr, "listen", "", "address to listen to (default: :8080)")
-	flag.StringVar(&s3Endpoint, "s3-endpoint", "", "s3 endpoint")
-	flag.StringVar(&s3Region, "s3-region", "", "s3 region")
-	flag.StringVar(&s3Bucket, "s3-bucket", "", "s3 bucket")
-	flag.StringVar(&s3AccessKey, "s3-access-key", "", "s3 access key")
-	flag.StringVar(&s3SecretKey, "s3-secret-key", "", "s3 secret key")
+	flag.StringVar(&s3.endpoint, "s3-endpoint", "", "s3 endpoint")
+	flag.StringVar(&s3.region, "s3-region", "", "s3 region")
+	flag.StringVar(&s3.bucket, "s3-bucket", "", "s3 bucket")
+	flag.StringVar(&s3.accessKey, "s3-access-key", "", "s3 access key")
+	flag.StringVar(&s3.secretKey, "s3-secret-key", "", "s3 secret key")
 }
 
 func main() {
@@ -55,54 +53,24 @@ func main() {
 		listenAddr = ":8080"
 	}
 
-	if len(s3Endpoint) == 0 {
-		s3Endpoint = os.Getenv("PUT_S3_ENDPOINT")
+	if len(s3.endpoint) == 0 {
+		s3.endpoint = os.Getenv("PUT_S3_ENDPOINT")
 	}
 
-	if len(s3Region) == 0 {
-		s3Region = os.Getenv("PUT_S3_REGION")
+	if len(s3.region) == 0 {
+		s3.region = os.Getenv("PUT_S3_REGION")
 	}
 
-	if len(s3Bucket) == 0 {
-		s3Bucket = os.Getenv("PUT_S3_BUCKET")
+	if len(s3.bucket) == 0 {
+		s3.bucket = os.Getenv("PUT_S3_BUCKET")
 	}
 
-	if len(s3AccessKey) == 0 {
-		s3AccessKey = os.Getenv("PUT_S3_ACCESS_KEY")
+	if len(s3.accessKey) == 0 {
+		s3.accessKey = os.Getenv("PUT_S3_ACCESS_KEY")
 	}
 
-	if len(s3SecretKey) == 0 {
-		s3SecretKey = os.Getenv("PUT_S3_SECRET_KEY")
-	}
-	s3Url, err := url.Parse(s3Endpoint)
-
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	s3Client, err := minio.New(s3Url.Host, s3AccessKey, s3SecretKey, s3Url.Scheme == "https")
-
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	ok, err := s3Client.BucketExists(s3Bucket)
-
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-	if !ok {
-		if err := s3Client.MakeBucket(s3Bucket, s3Region); err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-	}
-
-	if err := s3Client.SetBucketPolicy(s3Bucket, "", policy.BucketPolicyReadOnly); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+	if len(s3.secretKey) == 0 {
+		s3.secretKey = os.Getenv("PUT_S3_SECRET_KEY")
 	}
 	l, err := listen(listenAddr)
 
@@ -112,9 +80,12 @@ func main() {
 	}
 	defer l.Close()
 
+	metrics := NewMetrics()
+
 	mux := http.NewServeMux()
-	mux.Handle("/", NewRequestHandler(rootDir, s3Client, s3Endpoint, s3Bucket))
-	mux.Handle("/health", NewHealthHandler(s3Client, s3Bucket))
+	mux.Handle("/", NewRequestHandler(rootDir, &s3, metrics))
+	mux.Handle("/health", NewHealthHandler(&s3))
+	mux.Handle("/metrics", NewMetricsHandler(metrics))
 
 	go http.Serve(l, mux)
 
@@ -130,15 +101,94 @@ func listen(addr string) (net.Listener, error) {
 	return net.Listen("unix", addr)
 }
 
+type S3 struct {
+	minio     *minio.Client
+	url       *url.URL
+	endpoint  string
+	bucket    string
+	region    string
+	secretKey string
+	accessKey string
+}
+
+func (s3 *S3) Put(name, content_type string, body io.Reader) (string, error) {
+	client, err := s3.client()
+
+	if err != nil {
+		return "", err
+	}
+
+	if _, err := client.PutObject(s3.bucket, name, body, content_type); err != nil {
+		return "", err
+	}
+	s3.url.Path = path.Join(s3.bucket, name)
+	return s3.url.String(), nil
+}
+
+func (s3 *S3) HealthCheck() error {
+	client, err := s3.client()
+
+	if err != nil {
+		return err
+	}
+	ok, err := client.BucketExists(s3.bucket)
+
+	if err != nil {
+		return err
+	}
+
+	if !ok {
+		fmt.Errorf("bucket unavailable: %s", s3.bucket)
+	}
+	return nil
+}
+
+func (s3 *S3) client() (client *minio.Client, err error) {
+	if s3.minio != nil {
+		return s3.minio, nil
+	}
+	s3.url, err = url.Parse(s3.endpoint)
+
+	if err != nil {
+		return nil, err
+	}
+	client, err = minio.New(s3.url.Host, s3.accessKey, s3.secretKey, s3.url.Scheme == "https")
+
+	if err != nil {
+		return nil, err
+	}
+	ok, err := client.BucketExists(s3.bucket)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if !ok {
+		if err := client.MakeBucket(s3.bucket, s3.region); err != nil {
+			return nil, err
+		}
+	}
+
+	if err := client.SetBucketPolicy(s3.bucket, "", policy.BucketPolicyReadOnly); err != nil {
+		return nil, err
+	}
+
+	s3.minio = client
+	return client, nil
+}
+
 type RequestHandler struct {
-	s3Client   *minio.Client
-	s3Endpoint string
-	s3Bucket   string
+	s3         *S3
+	metrics    Metrics
 	fileServer http.Handler
 }
 
-func NewRequestHandler(rootDir string, s3Client *minio.Client, s3Endpoint, s3Bucket string) *RequestHandler {
-	return &RequestHandler{s3Client, s3Endpoint, s3Bucket, http.FileServer(http.Dir(rootDir))}
+func NewRequestHandler(rootDir string, s3 *S3, metrics Metrics) *RequestHandler {
+	return &RequestHandler{
+		s3:         s3,
+		metrics:    metrics,
+		fileServer: http.FileServer(http.Dir(rootDir)),
+	}
 }
 
 func (rh *RequestHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -146,57 +196,96 @@ func (rh *RequestHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		rh.fileServer.ServeHTTP(w, r)
 		return
 	}
+	rh.metrics.Total.Inc(1)
 
 	if r.Method != "POST" && r.Method != "PUT" {
-		http.Error(w, "page not found", 404)
+		rh.metrics.Invalid.Inc(1)
+		w.Header().Set("Allow", "GET, POST, PUT")
+		http.Error(w, "method not allowed", 405)
 		return
 	}
-	name := uuid.New()
+	objectUrl, err := rh.s3.Put(uuid.New(), r.Header.Get("Content-Type"), r.Body)
 
-	if _, err := rh.s3Client.PutObject(rh.s3Bucket, name, r.Body, r.Header.Get("Content-Type")); err != nil {
+	if err != nil {
+		rh.metrics.Failed.Inc(1)
 		fmt.Println("could not store object:", err)
 		http.Error(w, "internal server error", 500)
 		return
 	}
-	objectUrl, err := url.Parse(rh.s3Endpoint)
 
-	if err != nil {
-		fmt.Println("could not parse endpoint url:", err)
-		http.Error(w, "internal server error", 500)
+	if _, err := fmt.Fprintln(w, objectUrl); err != nil {
+		rh.metrics.Failed.Inc(1)
+		fmt.Println("could respond with object url:", err)
 		return
 	}
-	objectUrl.Path = path.Join(rh.s3Bucket, name)
-
-	if _, err := fmt.Fprintln(w, objectUrl.String()); err != nil {
-		fmt.Println("could respond with object url:", err)
-	}
+	rh.metrics.Success.Inc(1)
 }
 
 type HealthHandler struct {
-	s3Client *minio.Client
-	s3Bucket string
+	s3 *S3
 }
 
 type HealthPayload struct {
 	Status string `json:"status"`
 }
 
-func NewHealthHandler(s3Client *minio.Client, s3Bucket string) *HealthHandler {
-	return &HealthHandler{s3Client: s3Client, s3Bucket: s3Bucket}
+func NewHealthHandler(s3 *S3) *HealthHandler {
+	return &HealthHandler{s3: s3}
 }
 
 func (hh *HealthHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	payload := &HealthPayload{Status: "ok"}
-	ok, err := hh.s3Client.BucketExists(hh.s3Bucket)
 
-	if err != nil {
+	if err := hh.s3.HealthCheck(); err != nil {
 		payload.Status = fmt.Sprintf("critical: %s", err)
-	} else if !ok {
-		payload.Status = fmt.Sprintf("critical: bucket \"%s\" unavailable", hh.s3Bucket)
 	}
 	w.Header().Set("Content-Type", "application/json")
 
 	if err := json.NewEncoder(w).Encode(payload); err != nil {
 		fmt.Println("could not respond with health payload:", err)
+	}
+}
+
+type Counter struct {
+	metrics.Counter
+}
+
+func NewCounter() Counter {
+	return Counter{metrics.NewCounter()}
+}
+
+func (c Counter) MarshalJSON() ([]byte, error) {
+	return json.Marshal(c.Count())
+}
+
+type Metrics struct {
+	Total   Counter `json:"total"`
+	Invalid Counter `json:"invalid"`
+	Failed  Counter `json:"failed"`
+	Success Counter `json:"success"`
+}
+
+func NewMetrics() Metrics {
+	return Metrics{
+		Total:   NewCounter(),
+		Invalid: NewCounter(),
+		Failed:  NewCounter(),
+		Success: NewCounter(),
+	}
+}
+
+type MetricsHandler struct {
+	metrics Metrics
+}
+
+func NewMetricsHandler(metrics Metrics) *MetricsHandler {
+	return &MetricsHandler{metrics: metrics}
+}
+
+func (mh *MetricsHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	if err := json.NewEncoder(w).Encode(mh.metrics); err != nil {
+		fmt.Println("could not respond with metrics payload:", err)
 	}
 }
