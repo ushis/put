6f54594c69e695ade13d114923a2b27d5b972e52
ushi <ushi@honkgong.info>
2018-07-09 22:20:58 +0200

    do not setup bucket

--- putd.go
+++ putd.go
@@ -5,7 +5,6 @@ import (
 	"flag"
 	"fmt"
 	"github.com/minio/minio-go"
-	"github.com/minio/minio-go/pkg/policy"
 	"github.com/pborman/uuid"
 	"github.com/rcrowley/go-metrics"
 	"io"
@@ -20,18 +19,19 @@ import (
 )
 
 var (
-	rootDir    string
-	listenAddr string
-	s3         S3
+	listenAddr  string
+	s3Endpoint  string
+	s3Bucket    string
+	s3AccessKey string
+	s3SecretKey string
 )
 
 func init() {
 	flag.StringVar(&listenAddr, "listen", "", "address to listen to (default: :8080)")
-	flag.StringVar(&s3.endpoint, "s3-endpoint", "", "s3 endpoint")
-	flag.StringVar(&s3.region, "s3-region", "", "s3 region")
-	flag.StringVar(&s3.bucket, "s3-bucket", "", "s3 bucket")
-	flag.StringVar(&s3.accessKey, "s3-access-key", "", "s3 access key")
-	flag.StringVar(&s3.secretKey, "s3-secret-key", "", "s3 secret key")
+	flag.StringVar(&s3Endpoint, "s3-endpoint", "", "s3 endpoint")
+	flag.StringVar(&s3Bucket, "s3-bucket", "", "s3 bucket")
+	flag.StringVar(&s3AccessKey, "s3-access-key", "", "s3 access key")
+	flag.StringVar(&s3SecretKey, "s3-secret-key", "", "s3 secret key")
 }
 
 func main() {
@@ -45,39 +45,48 @@ func main() {
 		listenAddr = ":8080"
 	}
 
-	if len(s3.endpoint) == 0 {
-		s3.endpoint = os.Getenv("PUT_S3_ENDPOINT")
+	if len(s3Endpoint) == 0 {
+		s3Endpoint = os.Getenv("PUT_S3_ENDPOINT")
 	}
 
-	if len(s3.region) == 0 {
-		s3.region = os.Getenv("PUT_S3_REGION")
+	if len(s3Bucket) == 0 {
+		s3Bucket = os.Getenv("PUT_S3_BUCKET")
 	}
 
-	if len(s3.bucket) == 0 {
-		s3.bucket = os.Getenv("PUT_S3_BUCKET")
+	if len(s3AccessKey) == 0 {
+		s3AccessKey = os.Getenv("PUT_S3_ACCESS_KEY")
 	}
 
-	if len(s3.accessKey) == 0 {
-		s3.accessKey = os.Getenv("PUT_S3_ACCESS_KEY")
+	if len(s3SecretKey) == 0 {
+		s3SecretKey = os.Getenv("PUT_S3_SECRET_KEY")
 	}
+	url, err := url.Parse(s3Endpoint)
 
-	if len(s3.secretKey) == 0 {
-		s3.secretKey = os.Getenv("PUT_S3_SECRET_KEY")
+	if err != nil {
+		fmt.Println("could not parse s3 endpoint:", err)
+		os.Exit(1)
+	}
+	s3Client, err := minio.New(url.Host, s3AccessKey, s3SecretKey, url.Scheme == "https")
+
+	if err != nil {
+		fmt.Println("could not set up s3 client:", err)
+		os.Exit(1)
 	}
 	l, err := listen(listenAddr)
 
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("could not set up listener:", err)
 		os.Exit(1)
 	}
 	defer l.Close()
 
+	s3 := NewS3(s3Client, s3Endpoint, s3Bucket)
 	metrics := NewMetrics()
 
 	mux := http.NewServeMux()
-	mux.Handle("/", NewRequestHandler(&s3, metrics))
-	mux.Handle("/health", NewHealthHandler(&s3))
-	mux.Handle("/metrics", NewMetricsHandler(metrics, &s3))
+	mux.Handle("/", NewRequestHandler(s3, metrics))
+	mux.Handle("/health", NewHealthHandler(s3))
+	mux.Handle("/metrics", NewMetricsHandler(metrics, s3))
 
 	go http.Serve(l, mux)
 
@@ -94,37 +103,32 @@ func listen(addr string) (net.Listener, error) {
 }
 
 type S3 struct {
-	minio     *minio.Client
-	url       *url.URL
-	endpoint  string
-	bucket    string
-	region    string
-	secretKey string
-	accessKey string
+	client   *minio.Client
+	endpoint string
+	bucket   string
+}
+
+func NewS3(client *minio.Client, endpoint, bucket string) *S3 {
+	return &S3{client: client, endpoint: endpoint, bucket: bucket}
 }
 
 func (s3 *S3) Put(name, contentType string, body io.Reader) (string, error) {
-	client, err := s3.client()
+	url, err := url.Parse(s3.endpoint)
 
 	if err != nil {
 		return "", err
 	}
 	meta := minio.PutObjectOptions{ContentType: contentType}
 
-	if _, err := client.PutObject(s3.bucket, name, body, -1, meta); err != nil {
+	if _, err := s3.client.PutObject(s3.bucket, name, body, -1, meta); err != nil {
 		return "", err
 	}
-	s3.url.Path = path.Join(s3.bucket, name)
-	return s3.url.String(), nil
+	url.Path = path.Join(s3.bucket, name)
+	return url.String(), nil
 }
 
 func (s3 *S3) HealthCheck() error {
-	client, err := s3.client()
-
-	if err != nil {
-		return err
-	}
-	ok, err := client.BucketExists(s3.bucket)
+	ok, err := s3.client.BucketExists(s3.bucket)
 
 	if err != nil {
 		return err
@@ -137,51 +141,11 @@ func (s3 *S3) HealthCheck() error {
 }
 
 func (s3 *S3) Metrics() (count int64, size int64, err error) {
-	client, err := s3.client()
-
-	if err != nil {
-		return count, size, err
-	}
-
-	for obj := range client.ListObjects(s3.bucket, "", false, make(chan struct{})) {
+	for obj := range s3.client.ListObjects(s3.bucket, "", false, make(chan struct{})) {
 		count += 1
 		size += obj.Size
 	}
 	return count, size, err
-}
-
-func (s3 *S3) client() (client *minio.Client, err error) {
-	if s3.minio != nil {
-		return s3.minio, nil
-	}
-	s3.url, err = url.Parse(s3.endpoint)
-
-	if err != nil {
-		return nil, err
-	}
-	client, err = minio.New(s3.url.Host, s3.accessKey, s3.secretKey, s3.url.Scheme == "https")
-
-	if err != nil {
-		return nil, err
-	}
-	ok, err := client.BucketExists(s3.bucket)
-
-	if err != nil {
-		return nil, err
-	}
-
-	if !ok {
-		if err := client.MakeBucket(s3.bucket, s3.region); err != nil {
-			return nil, err
-		}
-	}
-
-	if err := client.SetBucketPolicy(s3.bucket, policy.BucketPolicyReadOnly); err != nil {
-		return nil, err
-	}
-
-	s3.minio = client
-	return client, nil
 }
 
 type RequestHandler struct {
